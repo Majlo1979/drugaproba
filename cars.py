c5c94008ea23f417d9f8b981692c1dc6c59a3b86
MiłoszKonieczny <miloszkonieczny79@gmail.com>
2019-02-01 19:33:07 +0100

    hej to mój pierwszy mini projekt

---
+++ cars.py
@@ -0,0 +1,27 @@
+cars = ['bmw', 'audi', 'citroen', 'ford', 'volkswagen']
+cars.sort()
+print(cars)
+
+###
+
+cars = ['bmw', 'audi', 'citroen', 'ford', 'volkswagen']
+cars.sort(reverse=True)
+print(cars)
+
+###
+
+cars = ['bmw', 'audi', 'citroen', 'ford', 'volkswagen']
+print("Oto lista początkowa: ")
+print(cars)
+
+print('\nOto lista posortowana: ')
+print(sorted(cars))
+
+print('Oto lista początkowa: ')
+print(cars)
+
+###
+
+cars = ['bmw', 'audi', 'citroen', 'ford', 'volkswagen']
+auta = len(cars)
+print(auta)
